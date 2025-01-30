f6ac44781ab2427c978af3c60a71dc48e62bf918
Khai <storage27592@gmail.com>
2025-01-30 16:59:45 +0900

    change function symbol

--- Haskell/SetTheory/Nat.hs
+++ Haskell/SetTheory/Nat.hs
@@ -1,24 +1,24 @@
-import Prelude hiding (succ)
+import Prelude hiding (succ, (+))
 
 
 class ArithmeticNat a where
     zero :: a
     succ :: a -> a
-    add  :: a -> a -> a
+    (+)  :: a -> a -> a
 
 
 data Nat = Zero | Succ Nat deriving (Eq, Show)
 instance ArithmeticNat Nat where
     zero = Zero
     succ = Succ
-    add x Zero = x
-    add x (Succ y) = Succ (add x y)
+    x + Zero = x
+    x + (Succ y) = Succ (x + y)
 
 
 main :: IO ()
 main = do
     let one  :: Nat = succ zero
     let two  :: Nat = succ one
-    let two' :: Nat = add one one
+    let two' :: Nat = one + one
     print $ two == two'
     print two'
